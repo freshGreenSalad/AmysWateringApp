9c917036f4a1b1a334e0231cbc7dc00acdc81583
conor Small <csma174@aucklanduni.ac.nz>
2023-09-25 20:11:57 +1300

    migrated from gradle grovy to koltin

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+/*pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "AmysWateringApp"
+include ':app'*/
+
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "AmysWateringApp"
+include(":app")
